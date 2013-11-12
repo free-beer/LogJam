e42cc2ffe1eb53e7975b5a48f1cbabc624fdc961
Peter Wood <pwood@blacknorth.com>
2013-11-12 10:04:35 +0000

    Added extra tests to the object unit test file.

--- test/unit/logjam_object_spec.rb
+++ test/unit/logjam_object_spec.rb
@@ -4,14 +4,27 @@
 
 require 'minitest/autorun'
 require 'minitest/spec'
+require 'logger'
 require 'logjam'
 
 describe Object do
    it "possesses a class level log method" do
       Object.respond_to?(:log).must_equal(true)
+      Object.log.class.must_equal(LogJam::LogJamLogger)
    end
 
    it "possesses an instance level log method" do
       "".respond_to?(:log).must_equal(true)
+      "".log.class.must_equal(LogJam::LogJamLogger)
+   end
+
+   it "possesses a class level log= method" do
+      Object.respond_to?(:log=).must_equal(true)
+   end
+
+   it "reassigns the logger when asked to do so" do
+      logger = Logger.new(STDERR)
+      Object.log = logger
+      Object.log.logger.must_equal(logger)
    end
 end
